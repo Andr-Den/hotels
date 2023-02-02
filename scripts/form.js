5ba7694c388fe9d4832f92d9a0cdb0f271806518
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2023-02-02 19:31:48 +0500

    add submit function

--- scripts/form.js
+++ scripts/form.js
@@ -1,6 +1,7 @@
 let openFormButton = document.getElementById("open-form");
 let form = document.getElementById("form");
 let closeFormButton = document.getElementById("close-form");
+let submitButton = document.getElementById("submit");
 
 openFormButton.onclick = function() {
   form.style.visibility = "visible"
@@ -8,4 +9,21 @@ openFormButton.onclick = function() {
 
 closeFormButton.onclick = function() {
   form.style.visibility = "hidden"
+}
+
+submitButton.onclick = function(e) {
+  e.preventDefault()
+
+  let request = new XMLHttpRequest();
+  let name = document.getElementById("name");
+  let phone = document.getElementById("phone");
+
+  let person = JSON.stringify({
+    name: name.value,
+    phone: phone.value
+  });
+  
+  request.open("POST", '/submit')
+  request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  request.send(person);
 }
